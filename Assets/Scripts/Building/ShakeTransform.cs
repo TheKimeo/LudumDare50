fd7957e8f89dd4d11134f128916ab721e8b71963
James Kime <jameskime100@googlemail.com>
2022-04-02 22:45:26 +0100

    Add ShakeTransform and SpinTransform
    The radar and the drill now look more animated!

---
+++ Assets/Scripts/Building/ShakeTransform.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ShakeTransform : MonoBehaviour
+{
+	[SerializeField] Transform m_TransformToSpin;
+	[SerializeField] Vector3 m_Axis = Vector3.up;
+	[SerializeField] float m_Speed = 2.0f;
+	[SerializeField] float m_Magnitude = 0.5f;
+
+	Vector3 m_StartingLocalPosition;
+
+	private void Start()
+	{
+		if ( m_TransformToSpin == null )
+		{
+			Debug.Assert( false, "[SpinTransform] No provided transform to spin for object: " + gameObject );
+			return;
+		}
+
+		m_StartingLocalPosition = m_TransformToSpin.localPosition;
+	}
+
+	private void Update()
+	{
+		float offset = ( Time.time * m_Speed ) % Mathf.PI;
+		m_TransformToSpin.localPosition = m_StartingLocalPosition + m_Axis * Mathf.Sin( offset ) * m_Magnitude;
+	}
+}
---
+++ Assets/Scripts/Building/SpinTransform.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SpinTransform : MonoBehaviour
+{
+	[SerializeField] Transform m_TransformToSpin;
+	[SerializeField] Vector3 m_Axis = Vector3.up;
+	[SerializeField] float m_Speed = 50.0f;
+
+	private void Start()
+	{
+		if ( m_TransformToSpin == null )
+		{
+			Debug.Assert( false, "[SpinTransform] No provided transform to spin for object: " + gameObject );
+			return;
+		}
+	}
+
+	private void Update()
+	{
+		m_TransformToSpin.rotation *= Quaternion.AngleAxis( m_Speed * Time.deltaTime, m_Axis );
+	}
+}

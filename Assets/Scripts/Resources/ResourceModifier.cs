033ffafd23a6486d293af1e0a62c7d3144ba50d8
James Kime <jameskime100@googlemail.com>
2022-04-03 13:05:15 +0100

    Apply operational ratio to ResourceModifiers

--- Assets/Scripts/Resources/ResourceModifier.cs
+++ Assets/Scripts/Resources/ResourceModifier.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class ResourceModifier : MonoBehaviour
 {
 	[SerializeField] BuildingType m_buildingType;
-	
+	[SerializeField] BuildingState m_buildingState;
 
 	float m_tSinceProd = 0.0f;
 
@@ -11,16 +11,22 @@ public class ResourceModifier : MonoBehaviour
 	{
 		m_tSinceProd += Time.deltaTime;
 
-		if (m_tSinceProd >= 1.0f )
+		if ( m_tSinceProd >= 1.0f )
 		{
-			foreach(BuildingType.Cost cost in m_buildingType.m_costData)
-            {
-				cost.m_resourceType.Modify(cost.m_runCost);
-            }
+			foreach ( BuildingType.Cost cost in m_buildingType.m_costData )
+			{
+				float modifyAmount = cost.m_runCost;
+				if ( modifyAmount > 0.0f )
+				{
+					//Only positive generation is effected by operational shutdown
+					modifyAmount *= m_buildingState.OperationalRatio;
+				}
+
+				cost.m_resourceType.Modify( modifyAmount );
+			}
 
 			m_tSinceProd = 0.0f;
 			return;
 		}
-
 	}
 }
